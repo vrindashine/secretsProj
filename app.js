b8ca06a0f4b817ab31550e2417a9cde83417a0fb
vrindashine <vrindashine25@gmail.com>
2023-06-29 14:12:32 +0530

    Level 1 - Username and Password,Level 2 - Encryption

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion:6
+const express=require('express');
+const ejs=require('ejs');
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+const app= express();
+
+const  encrypt=require('mongoose-encryption')
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+const userSchema= new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+const secret="Thisisourlittesecret.";
+userSchema.plugin(encrypt, { secret: secret ,encryptedFields: ['password'] });
+
+const User=new mongoose.model('User',userSchema)
+app.get('/',function(req,res)
+{
+    res.render("home");
+});
+
+app.get('/login',function(req,res)
+{
+    res.render('login')
+});
+app.get('/register',function(req,res)
+{
+    res.render('register')
+});
+
+app.post('/register',function(req,res)
+{
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+   newUser.save().then(function(err)
+    { 
+   
+         res.render('secrets');
+    }).catch(function(err)
+    {
+        console.log(err);
+    });
+});
+
+app.post('/login',function(req,res)
+{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    User.findOne({email:username}).then(function(foundUser)
+    {
+        if(foundUser.password===password)
+        {
+            res.render('secrets');
+        }
+    }).catch(function(err)
+    {
+        console.log(err);
+    });
+});
+app.listen(3000,function()
+{
+    console.log("server started on port 3000");
+});

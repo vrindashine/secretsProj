a90accba68dbff081e6f6012e7e64288cbe5b3dc
vrindashine <vrindashine25@gmail.com>
2023-07-03 11:16:41 +0530

    Level 6 - OAuth 2.0 & How to Implement Sign In with Google,Finishing Up the App - Letting Users Submit Secrets

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const mongoose=require('mongoose');
 const session = require('express-session');
 const passport=require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 const app= express();
 
 
@@ -34,23 +36,57 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB');
 
 const userSchema= new mongoose.Schema({
     email:String,
-    password:String
+    password:String,
+    googleId:String,
+    secret:String
 });
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 //userSchema.plugin(encrypt, { secret: process.env.API_KEY ,encryptedFields: ['password'] });
 
 const User=new mongoose.model('User',userSchema)
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+ 
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+  
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get('/',function(req,res)
 {
     res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+  );
+
+  app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get('/login',function(req,res)
 {
@@ -63,14 +99,14 @@ app.get('/register',function(req,res)
 
 app.get('/secrets',function(req,res)
 {
-    if(req.isAuthenticated())
+    User.find({"secret":{$ne:null}}).then(function(foundUsers)
     {
-        res.render('secrets');
-    }
-    else{
-        res.redirect('/login');
-    }
-})
+        res.render("secrets",{usersWithSecrets:foundUsers});
+    }).catch(function(err)
+    {
+        console.log(err);
+    });
+});
 app.get('/logout',function(req,res)
 {
     req.logout(function(err) {
@@ -78,6 +114,34 @@ app.get('/logout',function(req,res)
         res.redirect('/');
       });
 })
+ app.get('/submit',function(req,res)
+ {
+    if(req.isAuthenticated())
+    {
+        res.render('submit');
+    }
+    else{
+        res.redirect('/login');
+    }
+
+ })
+ app.post('/submit',function(req,res)
+ {
+    
+    console.log(req.user.id);
+    User.findById(req.user._id.toString()).then(function(foundUser)
+    {
+        foundUser.secret=req.body.secret;
+        foundUser.save().then(function()
+        {
+            res.redirect("/secrets");
+        })
+    }).catch(function(err)
+    {
+        console.log(err);
+    });
+    
+ });
 app.post('/register',function(req,res)
 {
    User.register({username:req.body.username},req.body.password,function(err,user)
